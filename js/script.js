4257e181c69448e9b59dd58aae569c2e9e90e7e9
DYagmur <30656517+DYagmur@users.noreply.github.com>
2023-07-26 23:59:13 +0300

    current card funciton added

--- js/script.js
+++ js/script.js
@@ -1,26 +1,59 @@
-const LEVELS = {
-    easy: { gridSize: 6, imagesCount: 3 },
-    medium: { gridSize: 16, imagesCount: 8 },
-    hard: { gridSize: 20, imagesCount: 10 }
-  };
-
-  
-
 //Items array
-
 const items = [
-    {'',0,URL},{'',1,URL}, {'',2,URL}, {'',3,URL}, {'',4,URL}, {'',5,URL}, {'',6,URL}, {'',7,URL}, {'',8,URL}, {'',9,URL}];
+    { id: "alien", url: "./img/alien1.png" },
+    { id: "alien2", url: "./img/alien2.png" },
+    { id: "asteroid", url: "./img/asteroid.png" },
+    { id: "astronaut", url: "./img/astronaut.png" },
+    { id: "earth", url: "./img/earth.png" },
+    { id: "meteor", url: "./img/meteor.png" },
+    { id: "galaxy", url: "./img/galaxy.png" },
+    { id: "planet", url: "./img/planet.png" },
+    { id: "rocket", url: "./img/rocket.png" },
+    { id: "ufo", url: "./img/ufo.png" }
+];
 
-
-// Variable to keep track of the game state
-
-let cards = [];
-let matchedCards = [];
-let flippedCards = [];
+function createPictures(number) {
+    let cards = [];
+    for (let i = 0; i < number; i++) {
+        cards.push(items[i]);
+        cards.push(items[i]);
+    }
+    cards = shuffleArray(cards);
+    createCards(cards);
+}
 
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
     }
-  }
+    return array;
+}
+
+let turn = 1;
+let currentCard = "default";
+function isPair(att) {
+    console.log(att);
+    console.log(currentCard);
+    let open = document.querySelectorAll(`[card=${att}]`);
+    let current = document.querySelectorAll(`[card=${currentCard}]`);
+    if (turn == 2) {
+        turn = 1;
+        if (currentCard === att) {
+            currentCard = "default";
+        } else {
+            console.log("diferentes");
+            setTimeout(() => {
+                for (let i = 0; i < 2; i++) {
+                    open[i].classList.remove("flip");
+                    current[i].classList.remove("flip");
+                    currentCard = "default";
+                }
+            }, 500);
+        }
+    } else {
+        turn = 2;
+        currentCard = att;
+        console.log(att);
+    }
+}
