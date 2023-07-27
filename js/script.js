9cd254630175b3e7373415eae153a7254ee8cffb
DYagmur <30656517+DYagmur@users.noreply.github.com>
2023-07-27 22:48:48 +0300

    added comments

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-//Items array
+// Items array
 const items = [
     { id: "alien", url: "./img/alien1.png" },
     { id: "alien2", url: "./img/alien2.png" },
@@ -12,6 +12,7 @@ const items = [
     { id: "ufo", url: "./img/ufo.png" },
 ];
 
+// Function to create pairs of pictures based on the provided number
 function createPictures(number) {
     let cards = [];
     let i = 0;
@@ -24,6 +25,7 @@ function createPictures(number) {
     createCards(cards);
 }
 
+// Function to shuffle the elements of an array
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
@@ -34,6 +36,8 @@ function shuffleArray(array) {
 
 let turn = 1;
 let currentCard = "default";
+
+// Function to handle card matching
 function isPair(att) {
     let open = document.querySelectorAll(`[card=${att}]`);
     let current = document.querySelectorAll(`[card=${currentCard}]`);
@@ -42,6 +46,7 @@ function isPair(att) {
         if (currentCard === att) {
             currentCard = "default";
         } else {
+            // Delay card flipping if the second card is not a match
             setTimeout(() => {
                 for (let i = 0; i < 2; i++) {
                     open[i].classList.remove("flip");
@@ -54,12 +59,14 @@ function isPair(att) {
         turn = 2;
         currentCard = att;
     }
+    // Check if the game is completed and perform necessary actions
     if (checkGameCompletion()) {
         stop();
         setScore();
     }
 }
 
+// Function to check if the game is completed
 function checkGameCompletion() {
     let matchedCards = document.querySelectorAll(".card");
     let completed = true;
